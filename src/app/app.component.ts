e98b00fc4ef01eeabf0c00353571fd4b25bea18c
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-09 15:34:50 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  // 2 Buttons approve/reject
   isApproved = true;
 
   approve(status) {
     this.isApproved = status;
+  }
+
+  // 3 Buttons highlight
+  highligtColor = 'white';
+
+  highligt(color: string) {
+    this.highligtColor = color;
   }
 }
