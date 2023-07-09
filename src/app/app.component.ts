24928fd98e79ed36023bb1fc3b2a2654154bdf56
nkunic <45877400+nkunic@users.noreply.github.com>
2023-07-09 17:01:58 +0200

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,4 +19,23 @@ export class AppComponent {
   highligt(color: string) {
     this.highligtColor = color;
   }
+
+  // 5 Buttons highlight (Number)
+  isClearDisabled = true;
+  chosenNumber = 1;
+
+  numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+  ];
+
+  calculate(divisibleBy: number) {
+    this.chosenNumber = divisibleBy;
+    if (divisibleBy === 1) {
+      this.isClearDisabled = true;
+    } else {
+      this.isClearDisabled = false;
+    }
+  }
 }
